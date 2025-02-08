b1eac6866a3f6dd92be37becebb293b54387e254
swietektomasz <swietek.tomasz@protonmail.com>
2025-02-08 21:46:04 +0100

    Play around with open-meteo

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,46 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+type LocationSearchRequest = {
+  name: string;
+  count: string;
+  format: string;
+  language: string;
+};
+
+interface TypedRequestBody<T> extends Request {
+  body: T;
+}
+
+app.get(
+  "/weather",
+  (req: TypedRequestBody<LocationSearchRequest>, res: Response) => {
+    const searchParams = new URLSearchParams({
+      count: "1",
+      format: "json",
+      language: "en",
+      name: "Pszczyna",
+    }).toString();
+
+    console.log(searchParams);
+
+    const locations = fetch(
+      "https://geocoding-api.open-meteo.com/v1/search?" + searchParams
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+
+    // Probably just return a list of possible locations
+
+    res.send("Hello World!");
+  }
+);
+
+// set up leveldb
+// save lat/lon under a name into db
+// get weather based on saved name, save weather into db
 
 app.listen(port, () => {
   console.log(`Weather app server listening on port ${port}`);
